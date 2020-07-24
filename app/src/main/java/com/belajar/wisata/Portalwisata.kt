796b1d17ec35fb5ff8da0f99f1ac6a0c3bfa8630
Zelvia Olga Maharani <zelviaolgamaharani98@gmail.com>
2020-07-24 02:57:08 +0100

    last

--- app/src/main/java/com/belajar/wisata/Portalwisata.kt
+++ app/src/main/java/com/belajar/wisata/Portalwisata.kt
@@ -22,28 +22,29 @@ class Portalwisata : AppCompatActivity() {
             WebAction()
         }
 
-        WebAction()
+//        WebAction()
     }
 
     fun WebAction() {
 
         webView = findViewById(R.id.webview) as WebView
-        webView.settings.javaScriptEnabled = true
+//        webView.settings.javaScriptEnabled = true
 //        webView.settings.setAppCacheEnabled(true)
         webView.loadUrl("http://www.lintasntt.com/")
         swipe.isRefreshing = true
+
         webView.webViewClient = object : WebViewClient() {
 
-            override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
-
-                AlertDialog.Builder(this@Portalwisata)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle("Internet Mati")
-                    .setMessage("Hidupkan Koneksi Internet !")
-                    .setNegativeButton("OK", null)
-                    .show()
-
-            }
+//            override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
+//
+//                AlertDialog.Builder(this@Portalwisata)
+//                    .setIcon(android.R.drawable.ic_dialog_alert)
+//                    .setTitle("Internet Mati")
+//                    .setMessage("Hidupkan Koneksi Internet !")
+//                    .setNegativeButton("OK", null)
+//                    .show()
+//
+//            }
 
             override fun onPageFinished(view: WebView, url: String) {
                 // do your stuff here
